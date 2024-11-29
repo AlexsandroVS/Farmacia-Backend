e40d7bbde7ed00ca3b8c1627f5413406f77af832
AlexsandroVS <alexsandro.valeriano1@gmail.com>
2024-11-29 09:47:10 -0500

    Settings

--- farmavida/settings.py
+++ farmavida/settings.py
@@ -86,10 +86,8 @@ WSGI_APPLICATION = 'farmavida.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default='postgresql://postgresql:postgresql@localhost/postgres', conn_max_age=600),
+    
 }
 
 
